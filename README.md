8c2b5bb07a12b30dc4fe6282bdd6a1452b454a09
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-08-31 23:37:09 -0600

    Initial Commmit .. Included Template Folders Structure, .gitignore, Readme.md, JavaScript File, CSS Main File, and Index.HTML

---
+++ README.md
@@ -0,0 +1,12 @@
+#Aug 28, 2020 Milestone 1: Front-End Project (Resume with Validating Contact Form)
+#
+Title: Front-End Project
+Purpose:Resume with Validating Contact Form
+Author: Amr Fouad
+
+Assignment: https://docs.google.com/document/d/1rTWkjAguu10WuYsdmlIpvv5Ukd0pZWbzlbpNctVKN4M/edit?usp=sharing
+
+Trello board: https://trello.com/b/AFdr0k2a
+
+GitHub Repo: https://github.com/TECHCareers-by-Manpower/milestone-1-front-end-resume-w-contact-form-3mrfouad.git
+
---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+/*
+----------------------------
+Front-End Project DOCSTRING
+----------------------------
+Title: Front-End Project
+Purpose:Resume with Validating Contact Form
+Author: Amr Fouad
+Date of last edit: september xx, 2020 xx:xx am
+*/
+/*
+*===============================================
+                HTML STRUCTURE
+*===============================================
+*/
+/*
+------------------------------------------------ 
+Define document elements (Tags)
+------------------------------------------------
+*/
+/* Divisions */
+/* Footer */
+/* Generic breakline */
+/* Headers */
+/* Form */
+/* Wrong Char List */
+/* Paragraphs */
+/* Images */
+/* 
+------------------------------------------------
+Assign elements parameters 
+------------------------------------------------
+*/
+/* Elements IDs */
+/* Elements Text Content */
+/* Others */
+/*
+------------------------------------------------ 
+Layout he page structure (skeleton)
+------------------------------------------------
+*/
+/* HTML Body */
+/* HTML DIVs */
+/* HTML Forms */
+/* HTML UL */
+/* HTML Footer */
+/*
+*==================================================
+                JS Logic
+*==================================================
+*/
+/* Global Variables */
+/* Start */
+/* Event listener */
+/* Functions*/
