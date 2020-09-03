947e55892da30075861ab7466ffb9946e951d84e
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-09-03 15:15:36 -0600

    Updated the form validation behaviour to red highlight and red border the fields with errors

--- js/scripts.js
+++ js/scripts.js
@@ -9,42 +9,6 @@ Author: Amr Fouad
 Date of last edit: september xx, 2020 xx:xx am
 */
 /*
-*===============================================
-                HTML STRUCTURE
-*===============================================
-*/
-/*
------------------------------------------------- 
-Define document elements (Tags)
-------------------------------------------------
-*/
-/* Divisions */
-/* Footer */
-/* Generic breakline */
-/* Headers */
-/* Form */
-/* Wrong Char List */
-/* Paragraphs */
-/* Images */
-/* 
-------------------------------------------------
-Assign elements parameters 
-------------------------------------------------
-*/
-/* Elements IDs */
-/* Elements Text Content */
-/* Others */
-/*
------------------------------------------------- 
-Layout he page structure (skeleton)
-------------------------------------------------
-*/
-/* HTML Body */
-/* HTML DIVs */
-/* HTML Forms */
-/* HTML UL */
-/* HTML Footer */
-/*
 *==================================================
                 JS Logic
 *==================================================
@@ -53,13 +17,13 @@ Layout he page structure (skeleton)
 /* Start */
 /* Event listener */
 /* Functions*/
-
 /*
 *==================================================
             GLOBAL VARIABLES DEFINITION
 *==================================================
 */
 
+/* DOM ELEMENTS DEFINITION */
 const getInTouchForm = document.getElementById("contact-form");
 const name = document.getElementById("name");
 const emailID = document.getElementById("emailid");
@@ -85,7 +49,7 @@ document.body.onload = InitializePage();
                 CARDS ACCORDION LOGIC
 *==================================================
 */
-
+/* EVENT LISTENER */
 document.querySelectorAll(".accordion-btn").forEach((button) => {
   button.addEventListener("click", () => {
     button.classList.toggle("accordion-btn-active");
@@ -97,11 +61,11 @@ document.querySelectorAll(".accordion-btn").forEach((button) => {
             MAILTO VIA CONTACT FORM LOGIC
 *==================================================
 */
-
+/* EVENT LISTENER */
 getInTouchForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  if (GetInTouchFormValidation() == 0) {
-    const emailSubjectMod = `${subject.value} [From: ${name.value}]`;
+  if (GetInTouchFormValidation()) {
+    const emailSubjectMod = `${subject.value} __ From: ${name.value}`;
     window.location.href = `mailto:${emailID.value}&subject=${emailSubjectMod}&body=${message.value}`;
   }
 });
@@ -127,37 +91,46 @@ function GetInTouchFormValidation() {
   const swearWords = ["feldercarb", "frack", "skinjob", "vulgacarb"];
   let errorsCntr = 0;
   let swearingCntr = 0;
-
+  /* EMPTY FORM FIELDS VALIDATION */
   if (name.value == "") {
     nameErrorMsg.style.display = "block";
+    name.classList.add("empty-field-error");
     errorsCntr++;
   } else {
     nameErrorMsg.style.display = "none";
+    name.classList.remove("empty-field-error");
   }
   if (emailID.value == "") {
     emailErrorMsg.style.display = "block";
+    emailID.classList.add("empty-field-error");
     errorsCntr++;
   } else {
     emailErrorMsg.style.display = "none";
+    emailID.classList.remove("empty-field-error");
   }
   if (subject.value == "") {
     subjectErrorMsg.style.display = "block";
+    subject.classList.add("empty-field-error");
     errorsCntr++;
   } else {
     subjectErrorMsg.style.display = "none";
+    subject.classList.remove("empty-field-error");
   }
   if (message.value == "") {
     messageErrorMsg.style.display = "block";
+    message.classList.add("empty-field-error");
+
     errorsCntr++;
   } else {
     messageErrorMsg.style.display = "none";
+    message.classList.remove("empty-field-error");
   }
-
+  /* TEXT CONTAINS SWEARWORDS VALIDATION */
   for (word of swearWords) {
     if (
-      message.value.includes(word)||
-      name.value.includes(word)||
-      emailID.value.includes(word)||
+      message.value.includes(word) ||
+      name.value.includes(word) ||
+      emailID.value.includes(word) ||
       subject.value.includes(word)
     ) {
       swearingCntr++;
@@ -169,18 +142,19 @@ function GetInTouchFormValidation() {
       "This is a professional website, and use of inappropriate words won't be tolerated"
     );
     errorsCntr++;
-  }
-  else{
+  } else {
     swearWordErrorMsg.style.display = "none";
   }
-  if(!ValidateEmail()){
+  /* EMAIL ID FORMAT VALIDATION */
+  if (!ValidateEmail()) {
     emailFormatErrorMsg.style.display = "block";
+    emailID.classList.add("empty-field-error");
     errorsCntr++;
-  }
-  else{
+  } else {
     emailFormatErrorMsg.style.display = "none";
+    emailID.classList.remove("empty-field-error");
   }
-  return errorsCntr;
+  return Boolean(errorsCntr == 0);
 }
 
 /*
@@ -189,30 +163,32 @@ function GetInTouchFormValidation() {
 *==================================================
 */
 
-function ValidateEmail() 
-{
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailID.value))
-  {
-    return (true)
+function ValidateEmail() {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(emailID.value)) {
+    return true;
   }
-    return (false)
+  return false;
 }
 
 /**
  * Citation: Borrowed code to validate the email ID format.
  * Source: https://www.w3resource.com/javascript/form/email-validation.php
  * Reason: I didn't know a straight forward method and while researching found this simple solution.
- * How it works? It compares the two email id parts around the @ symbol. 
- * the id part may be up to 64 characters long and domain name may be up to 253 characters. 
- * 
+ * How it works? It compares the two email id parts around the @ symbol.
+ * the id part may be up to 64 characters long and domain name may be up to 253 characters.
+ *
  * The function checks the id part if contains the following ASCII characters:
  * Uppercase (A-Z) and lowercase (a-z) English letters.
  * Digits (0-9).
  * Characters ! # $ % & ' * + - / = ? ^ _ ` { | } ~
  * Character . ( period, dot or fullstop) provided that it is not the first or last character and it will not come one after the other.
- * 
- * The domain name [for example com, org, net, in, us, info] part contains letters, digits, hyphens, and dots. 
- * 
+ *
+ * The domain name [for example com, org, net, in, us, info] part contains letters, digits, hyphens, and dots.
+ *
+ * More details: I tried to understand more about regular expressions but apparantely it is a mini programming
+ * language by itself, I have skimmed through O'REILLY book called "Introducin Regular Expressions" and I got the
+ * primary idea, I guess I will need more time if mastering regular expressions so I can write codes on my own.
+ *
+ * Alternative solution: I could have written alternative code to validate the email ID using regular JavaScript
+ * loops and decision syntax but it would have been much longer of a code.
  */
-
-
