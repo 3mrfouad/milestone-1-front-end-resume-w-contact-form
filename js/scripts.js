2c3384199572261856fb54a566e965a723bd8c2e
Amr Fouad <68440701+3mrfouad@users.noreply.github.com>
2020-09-06 13:09:36 -0600

    Added map for international experience, core competencies setions

--- js/scripts.js
+++ js/scripts.js
@@ -57,9 +57,17 @@ document.querySelectorAll(".accordion-btn").forEach((button) => {
 /* EVENT LISTENER */
 getInTouchForm.addEventListener("submit", (event) => {
   event.preventDefault();
+  console.log(message.value.toString());
   if (GetInTouchFormValidation()) {
-    const emailSubjectMod = `${subject.value} __ From: ${name.value}`;
-    window.location.href = `mailto:${emailID.value}&subject=${emailSubjectMod}&body=${message.value}`;
+    const emailMessageMod = encodeURIComponent(
+      `From: ${name.value}` +
+        "\13" +
+        `Email: ${subject.value}` +
+        "\13Message:\13" +
+        `${message.value}`
+    );
+    const myEmail = "info@noemail.com";
+    window.location.href = `mailto:${myEmail}?subject=${subject.value}&body=${emailMessageMod}`;
   }
 });
 
@@ -202,8 +210,47 @@ closeBtn.addEventListener("click", closeNav);
 openBtn.addEventListener("click", openNav);
 
 function closeNav() {
-  document.getElementById("overlay-nav").style.height  = "0%";
+  document.getElementById("overlay-nav").style.height = "0%";
 }
 function openNav() {
-  document.getElementById("overlay-nav").style.height  = "100%";
+  document.getElementById("overlay-nav").style.height = "100%";
+}
+/*
+*==================================================
+            SLIDE SHOW LOGIC
+*==================================================
+*/
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display =
+    "flex"; /* changed from block to flex to test actual text not img */
+  dots[slideIndex - 1].className += " active";
 }
